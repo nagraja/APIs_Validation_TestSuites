5486f8b749dc1d541dc397fb191503264d3bc452
Nag Mothukuri <nag.mothukuri@ustpalu1670.local>
2021-09-24 16:32:55 -0400

    Updated Batch File

--- APIs_TestSuites_TemplateProject_BatchFiles/BatchFile_ToExecute_API_TestSuites.sh
+++ APIs_TestSuites_TemplateProject_BatchFiles/BatchFile_ToExecute_API_TestSuites.sh
@@ -13,12 +13,14 @@ then
     echo Hello Enter
     read -p 'Enter Environment URL Please: ' Env_URL
     read -p 'Enter Test Type (ST or FT) Please: ' TT_Value
+    Script_Location=~/.jenkins/workspace/API_TestSuites_Jenkins_And_Github_Integration_Job/ProjectName_APIs_TestSuites/
 else
-    echo Hello Don Not Enter
+    echo Hello Do Not Enter
     Env_URL=${Env_URL_Value}
     echo Environment URL Value From Jenkins is: $Env_URL
     TT_Value=${TestType_Value}
     echo TestType Value From Jenkins is: $TT_Value
+    Script_Location=~/Desktop/APIs_Validation_Script_and_Files/ProjectName_APIs_TestSuites
 fi
 
 case $TT_Value in
@@ -33,5 +35,7 @@ esac
 ####Environment URL Check Ends#####
 echo User Environment URL Value is: $Env_URL
 echo Test Type Value is: $TT_Value
+echo Script Location is: $Script_Location
+
 cd ${Script_Location} 
 mvn test -DTestSuiteName=${TestSuiteName} -DEnvURL=${Env_URL} -DTestType=${TT_Value}
